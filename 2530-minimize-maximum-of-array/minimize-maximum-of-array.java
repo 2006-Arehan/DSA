33ee2e57f6c389bd1b2cc530f3f1c136d5bca88b
Arehan Jain <arehanjain@gmail.com>
2026-07-14 03:39:25 +0530

    Time: 4 ms (99.40%) | Memory: 80.4 MB (28.61%) - LeetSync

---
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        long sum =0;
+        long ans =0;
+
+        for(int i =0;i<nums.length;i++){
+            sum+=nums[i];
+            long required = (sum+i)/(i+1);
+            ans = Math.max(ans,required);
+        }
+        return (int)ans;
+        
+    }
+}
